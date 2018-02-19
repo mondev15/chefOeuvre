f77f71090c545f228ac3a6d78c852ef734da6eda
mondev15 <mutonievergiste@gmail.com>
2018-02-19 15:42:49 +0100

    ajout de la taille fixe de la vue radar (à modifier)

--- chefOeuvre/src/test/Test.java
+++ chefOeuvre/src/test/Test.java
@@ -1,7 +1,5 @@
 package test;
 
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 import model.Plane;
@@ -13,30 +11,29 @@ public final class Test extends JFrame {
 
     public Test() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(420, 430);
+        this.setSize(430, 450);
+        this.setResizable(false);
         this.setLayout(null);
         this.setTitle("Plane Centered View");
 
         radar = new Radar();
-        radar.setBounds(0, 0, 400, 400);
-        radar.setSize(new Dimension(400, 400));
         
-        
-        Plane p0 = new Plane("p0",193, 195,0,0);
+        //---
+        Plane p0 = new Plane("DAL71",213, 213,0.f,0.f);
         radar.addPlaneToRadar(p0);
-
-        Plane p1 = new Plane("p1",75, 75, .1f, 0.0f);        
+        //---
+        Plane p1 = new Plane("SAB5458",75, 75, .1f, 0.0f);        
         radar.addPlaneToRadar(p1);
 
         //---
-        Plane p2 = new Plane("p2",200, 300, 0.1f, -.1f);
+        Plane p2 = new Plane("AF360UD",200, 300, 0.1f, -0.1f);
 
         radar.addPlaneToRadar(p2);
         //---
-        Plane p3 = new Plane("p3",250, 150, 0.1f, .2f);
+        Plane p3 = new Plane("RAM5712",250, 150, 0.1f, 0.2f);
         radar.addPlaneToRadar(p3);
         //---
-        Plane p4 = new Plane("p4",90, 105, -.1f, .2f);
+        Plane p4 = new Plane("AF118AB",90, 105, -0.1f, 0.2f);
         radar.addPlaneToRadar(p4);
 
         this.getContentPane().add(radar);
--- chefOeuvre/src/view/Radar.java
+++ chefOeuvre/src/view/Radar.java
@@ -2,62 +2,69 @@ package view;
 
 import model.Plane;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JPanel;
 
-public final class Radar extends JPanel {	
+public final class Radar extends JPanel {
 
-	private final List<Plane> planeList;
-	
-	public Radar(){		
-		planeList = new ArrayList<>();
-	}
-	
-	public void addPlaneToRadar(Plane p){
-		planeList.add(p);
-	}
-	
-	public void radarScan(){
-		for (Plane p : planeList) {
-			p.movePlane();
-		}					
-		repaint();
-	}
-	
-	@Override
-	public void paint(Graphics g) {	
-		
-		Graphics2D g2d = (Graphics2D)g;
-                //---
-                g2d.setRenderingHint(
-		        RenderingHints.KEY_ANTIALIASING,
-		        RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		g2d.setColor(Color.BLACK);
-		g2d.fillRect(0, 0, getWidth(), getHeight());		
-		
-		g2d.setColor(Color.GREEN);
-                //---
-		//g2d.drawArc(20, 20, 360,360,0, 180);
-                g2d.drawOval(20, 20, 360,360);
-		//---
-		g2d.drawLine(200, 20, 200, 380);
-		g2d.drawLine(20, 200, 380, 200);
-		
-		for (Plane p : planeList) {
-			p.drawPlane(g2d);
-                }
-				
-	}
-        
-        public List<Plane> getPlaneList(){
-          return planeList;
+    private final List<Plane> planeList;
+    private final int RADAR_SIZE = 400;
+    private final Point POINT = new Point(10,10);
+
+    public Radar() {
+        planeList = new ArrayList<>();
+        this.setSize(new Dimension(RADAR_SIZE,RADAR_SIZE));
+        this.setBounds(0, 0, 430, 430);
+    }
+
+    public void addPlaneToRadar(Plane p) {
+        planeList.add(p);
+    }
+
+    public void radarScan() {
+        for (Plane p : planeList) {
+            p.movePlane();
         }
-        
-        
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        //---
+        g2d.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+
+        g2d.setColor(Color.DARK_GRAY);
+        //---
+        //g2d.drawArc(20, 20, 360,360,0, 180);
+        g2d.drawOval(POINT.x+40, POINT.y+40,RADAR_SIZE-60, RADAR_SIZE-60);
+        g2d.drawOval(POINT.x+70, POINT.y+70, RADAR_SIZE-120, RADAR_SIZE-120);
+        g2d.drawOval(POINT.x+100, POINT.y+100, RADAR_SIZE-180, RADAR_SIZE-180);
+        //---
+        g2d.drawLine(220, 40, 220,400);
+        g2d.drawLine(40, 220, 400,220);
+
+        for (Plane p : planeList) {
+            p.drawPlane(g2d);
+        }
+
+    }
+
+    public List<Plane> getPlaneList() {
+        return planeList;
+    }
+
 }
